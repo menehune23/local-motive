d8fd50b28b779cf048b9d915d67da29cd8b3c5ee
Andrew Meyer <andrewm1100@gmail.com>
2017-09-16 23:04:24 -0500

    Fix issue with potentially conflicting field names across different models

--- lib/decorators.ts
+++ lib/decorators.ts
@@ -31,7 +31,13 @@ export function LocalStorage(
   deserialize?: (_: string) => any
 ) {
   return (target: any, name: string) => {
-    fieldConfigs[name] = new FieldConfig(false, key, defaultValue, cache, serialize, deserialize);
+    const t = target.constructor.name;
+
+    if (!fieldConfigs[t]) {
+      fieldConfigs[t] = {};
+    }
+
+    fieldConfigs[t][name] = new FieldConfig(false, key, defaultValue, cache, serialize, deserialize);
   }
 }
 
@@ -55,62 +61,72 @@ export function SessionStorage(
   deserialize?: (_: string) => any
 ) {
   return (target: any, name: string) => {
-    fieldConfigs[name] = new FieldConfig(true, key, defaultValue, cache, serialize, deserialize);
+    const t = target.constructor.name;
+
+    if (!fieldConfigs[t]) {
+      fieldConfigs[t] = {};
+    }
+
+    fieldConfigs[t][name] = new FieldConfig(true, key, defaultValue, cache, serialize, deserialize);
   }
 }
 
 export abstract class LocalModel {
 
-    constructor(private path: string) {
-      for (let field in fieldConfigs) {
-        addProperty(this, field, fieldConfigs[field]);
+  constructor(private path: string) {
+    const t = (<any>this.constructor).name;
+
+    if (fieldConfigs.hasOwnProperty(t)) {
+      for (let field in fieldConfigs[t]) {
+        addProperty(this, field, fieldConfigs[t][field]);
       }
     }
-
-    /**
-     * Generates a storage subpath, relative to this model's path.
-     * @param path Path to place under this model's path.
-     * @param index Optional index value for generating an indexed path.
-     */
-    subpath(path: string, index?: number): string {
-      return `${this.path}/${path}${(index != null) ? `/${index}` : ''}`;
-    }
-
-    /**
-     * Stores a value in local or session storage.
-     * @param key Storage key, relative to this model's storage path.
-     * @param value Value to store.
-     * @param session Whether or not to store in session storage.
-     * Defaults to false.
-     */
-    store(key: string, value: string, session: boolean = false) {
-      this.getStorage(session).setItem(this.subpath(key), value);
-    }
-
-    /**
-     * Retrieves a value from local or session storage.
-     * @param key Storage key, relative to this model's storage path.
-     * @param session Whether or not to store in session storage.
-     * Defaults to false.
-     */
-    load(key: string, session: boolean = false): string {
-      return this.getStorage(session).getItem(this.subpath(key));
-    }
-
-    /**
-     * Deletes a value from local or session storage.
-     * @param key Storage key, relative to this model's storage path.
-     * @param session Whether or not to delete from session storage.
-     * Defaults to false.
-     */
-    delete(key: string, session: boolean = false) {
-      this.getStorage(session).removeItem(this.subpath(key));
-    }
-
-    private getStorage(session: boolean): any {
-      return session ? sessionStorage : localStorage;
-    }
   }
+
+  /**
+   * Generates a storage subpath, relative to this model's path.
+   * @param path Path to place under this model's path.
+   * @param index Optional index value for generating an indexed path.
+   */
+  subpath(path: string, index?: number): string {
+    return `${this.path}/${path}${(index != null) ? `/${index}` : ''}`;
+  }
+
+  /**
+   * Stores a value in local or session storage.
+   * @param key Storage key, relative to this model's storage path.
+   * @param value Value to store.
+   * @param session Whether or not to store in session storage.
+   * Defaults to false.
+   */
+  store(key: string, value: string, session: boolean = false) {
+    this.getStorage(session).setItem(this.subpath(key), value);
+  }
+
+  /**
+   * Retrieves a value from local or session storage.
+   * @param key Storage key, relative to this model's storage path.
+   * @param session Whether or not to store in session storage.
+   * Defaults to false.
+   */
+  load(key: string, session: boolean = false): string {
+    return this.getStorage(session).getItem(this.subpath(key));
+  }
+
+  /**
+   * Deletes a value from local or session storage.
+   * @param key Storage key, relative to this model's storage path.
+   * @param session Whether or not to delete from session storage.
+   * Defaults to false.
+   */
+  delete(key: string, session: boolean = false) {
+    this.getStorage(session).removeItem(this.subpath(key));
+  }
+
+  private getStorage(session: boolean): any {
+    return session ? sessionStorage : localStorage;
+  }
+}
 
 function addProperty(target: LocalModel, name: string, config: FieldConfig) {
   const valueContainer: any = {};
